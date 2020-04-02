89584147de310cea4fd13e225145e43f4ffaa95b
Ola <temioladara2020@gmail.com>
2020-04-02 17:32:05 +0100

    to move code

--- src/test/java/com/nastygal/hooks/Hook.java
+++ src/test/java/com/nastygal/hooks/Hook.java
@@ -4,18 +4,15 @@ import com.nastygal.common.BrowserClass;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
-public class Hook extends BrowserClass
-{
+public class Hook extends BrowserClass {
     @Before
-    public void setup()
-    {
+    public void setup() {
         launchBrowswer("Chrome");
     }
 
-
     @After
-    public void tearDown()
-    {
+    public void tearDown() {
         closeBrowser();
     }
+
 }
